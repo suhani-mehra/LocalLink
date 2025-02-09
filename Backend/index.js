7ef5b8d231f9163d9bff5216fb6855f950147b16
SaatvikKabra10 <1saatvikkabra1@gmail.com>
2025-02-08 20:38:53 -0500

    saatvik

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+// Import Firebase functions
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyC8qko5QpGQqK2yUNM2bH04ONxxqRk4IzU",
+  authDomain: "locallink-3489c.firebaseapp.com",
+  projectId: "locallink-3489c",
+  storageBucket: "locallink-3489c.appspot.com",  // Fixed typo
+  messagingSenderId: "943700243297",
+  appId: "1:943700243297:web:0d119a9522cab05b8b4b20",
+  measurementId: "G-VDN7MMCLBN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firestore initialized:", db);
